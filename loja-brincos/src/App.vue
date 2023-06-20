aebe6a0e1140f5474bb57a826042b21a6d3b6905
Arto <myemail>
2023-06-20 01:09:01 -0300

    feat: inicio navbar

--- loja-brincos/src/App.vue
+++ loja-brincos/src/App.vue
@@ -1,23 +1,71 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+export default {
+  data: () => ({
+    tab: null,
+    colapsarBarra: false,
+  }),
+  methods: {
+    onResize(){
+      this.colapsarBarra = window.innerWidth < 768;
+      console.log(this.colapsarBarra)
+    }
+  },
+}
 </script>
 
 <template>
-
+  <v-app>
+    <v-app-bar
+      v-resize="onResize"
+      flat
+      color="red-darken-3"
+      class="d-flex justify-space-between"
+    >
+      <v-img
+        :height="50"
+        :max-width="200"
+        :min-width="200"
+        src="https://logodownload.org/wp-content/uploads/2014/09/google-logo-1-768x251.png"
+      >
+      </v-img>
+      <v-spacer></v-spacer>
+      <v-expand-transition>
+        <v-tabs
+          v-if="!colapsarBarra"
+          v-model="tab"
+        >
+          <v-tab>Brincos</v-tab>
+          <v-tab>Pulseiras</v-tab>
+          <v-tab>Personalização</v-tab>
+        </v-tabs>
+      </v-expand-transition>
+      <v-expand-transition>
+        <v-app-bar-nav-icon
+          icon="mdi-dots-vertical"
+          v-if="colapsarBarra"
+        >
+          ...
+        </v-app-bar-nav-icon>
+      </v-expand-transition>
+    </v-app-bar>
+    <v-window v-model="tab">
+      <v-window-item v-for="n in 3" :key="n" :value="n">
+        <v-container fluid>
+          <v-row>
+            <v-col v-for="i in 6" :key="i" cols="12" md="4">
+              <v-img
+                :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
+                :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
+                aspect-ratio="1"
+              ></v-img>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-window-item>
+    </v-window>
+  </v-app>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
